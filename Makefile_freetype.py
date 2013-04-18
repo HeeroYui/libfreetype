c04d149f7ff4e07f8b30a868e8dc634eaa3f2de2
Edouard DUPIN <yui.heero@gmail.com>
2013-04-18 21:07:17 +0200

    [DEV] corection of the python builder

--- Makefile_freetype.py
+++ Makefile_freetype.py
@@ -7,14 +7,24 @@ import buildTools
 myModule = module.module(__file__, 'freetype', 'LIBRARY')
 
 myModule.AddSrcFile([
-	'bullet/src/LinearMath/btQuickprof.cpp',
-	'bullet/src/LinearMath/btGeometryUtil.cpp',
-	'bullet/src/LinearMath/btAlignedAllocator.cpp',
-	'bullet/src/LinearMath/btSerializer.cpp',
-	'bullet/src/LinearMath/btConvexHull.cpp',
-	'bullet/src/LinearMath/btPolarDecomposition.cpp',
-	'bullet/src/LinearMath/btVector3.cpp',
-	'bullet/src/LinearMath/btConvexHullComputer.cpp'])
+	'freetype/base/ftbbox.c',
+	'freetype/base/ftbitmap.c',
+	'freetype/base/ftglyph.c',
+	'freetype/base/ftstroke.c',
+	'freetype/base/ftxf86.c',
+	'freetype/base/ftbase.c',
+	'freetype/base/ftsystem.c',
+	'freetype/base/ftinit.c',
+	'freetype/base/ftgasp.c',
+	'freetype/base/ftadvanc.c',
+	'freetype/raster/raster.c',
+	'freetype/sfnt/sfnt.c',
+	'freetype/smooth/smooth.c',
+	'freetype/autofit/autofit.c',
+	'freetype/truetype/truetype.c',
+	'freetype/cff/cff.c',
+	'freetype/psnames/psnames.c',
+	'freetype/pshinter/pshinter.c'])
 
 myModule.CompileFlags_CC([
 	'-W',
@@ -27,6 +37,7 @@ myModule.CompileFlags_CC([
 myModule.AddExportPath(buildTools.GetCurrentPath(__file__))
 myModule.AddPath(buildTools.GetCurrentPath(__file__)+"/freetype/")
 myModule.AddPath(buildTools.GetCurrentPath(__file__)+"/freetype/internal")
+myModule.AddPath(buildTools.GetCurrentPath(__file__)+"/freetype/internal/services")
 myModule.AddPath(buildTools.GetCurrentPath(__file__)+"/freetype/psaux")
 myModule.AddPath(buildTools.GetCurrentPath(__file__)+"/freetype/pshinter")
 myModule.AddPath(buildTools.GetCurrentPath(__file__)+"/freetype/psnames")
